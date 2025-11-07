9a250d46b30f654c2ae51f788e9f62a6bbcf1a19
Smapifan <luca.vogel@gski.de>
2025-11-07 20:32:33 +0100

    Delete docs/api directory

--- docs/api/cleanup.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  globalThis.games = globalThis.games || {};
-  const now = Date.now();
-  for (const id in globalThis.games) {
-    if (now - globalThis.games[id].created > 3 * 60 * 1000) delete globalThis.games[id];
-  }
-  return res.status(200).json({ message: "Old games removed" });
-}
